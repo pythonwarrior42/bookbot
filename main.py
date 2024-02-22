2400b104651d7d20d111061f31f7b7d81f0cfe0e
= <=>
2024-02-22 23:06:59 +0000

    modified functions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def main():
     book_path = "github.com/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
-
+    words = read_words(text)
+    
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def read_words(text):
+    words = []
+    words = text.split()
+    return words
 
+main()
